7888b6a472a46b2647ca3f478149bff381941b2e
Dovhan_Eduard <dovhan.eduard@gmail.com>
2024-06-10 20:36:19 +0300

    add first and second tasks

---
+++ src/js/1-timer.js
@@ -0,0 +1,99 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const startBtn = document.querySelector('button');
+const dateTimePickerInput = document.querySelector('#datetime-picker');
+const spanDays = document.querySelector('[data-days]');
+const spanHours = document.querySelector('[data-hours]');
+const spanMinutes = document.querySelector('[data-minutes]');
+const spanSeconds = document.querySelector('[data-seconds]');
+
+startBtn.disabled = true;
+
+let userSelectedDate;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const currentDate = Date.now();
+    const chosenDate = selectedDates[0].getTime();
+
+    if (chosenDate < currentDate) {
+      iziToast.show({
+        messageColor: '#fff',
+        backgroundColor: 'red',
+        position: 'topRight',
+        message: 'Please choose a date in the future',
+      });
+    } else {
+      userSelectedDate =
+        new Date(dateTimePickerInput.value).getTime() - currentDate;
+
+      startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr(dateTimePickerInput, options);
+
+startBtn.addEventListener('click', () => {
+  dateTimePickerInput.disabled = true;
+  startBtn.disabled = true;
+
+  let intervalId = setInterval(() => {
+    if (userSelectedDate > 0) {
+      let convertedDate = convertMs(userSelectedDate);
+
+      const time = addLeadingZero(convertedDate);
+
+      spanDays.innerHTML = time.days;
+      spanHours.innerHTML = time.hours;
+      spanMinutes.innerHTML = time.minutes;
+      spanSeconds.innerHTML = time.seconds;
+
+      userSelectedDate -= 1000;
+    } else {
+      clearInterval(intervalId);
+
+      dateTimePickerInput.disabled = false;
+      startBtn.disabled = false;
+    }
+  }, 1000);
+});
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(time) {
+  time.days = time.days.toString().padStart(2, '0');
+  time.hours = time.hours.toString().padStart(2, '0');
+  time.minutes = time.minutes.toString().padStart(2, '0');
+  time.seconds = time.seconds.toString().padStart(2, '0');
+
+  return time;
+}
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,50 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const inputDelay = form.elements.delay;
+const radioBtnFulfilled = form.querySelector('[value="fulfilled"]');
+const radioBtnRejected = form.querySelector('[value="rejected"]');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = inputDelay.value;
+
+  const state = radioBtnFulfilled.checked;
+
+  promiseGenerator(delay, state)
+    .then(ms => {
+      iziToast.show({
+        messageColor: '#fff',
+        backgroundColor: 'teal',
+        position: 'topRight',
+        message: `✅ Fulfilled promise in ${ms}ms`,
+      });
+    })
+    .catch((ms) => {
+      iziToast.show({
+        messageColor: '#fff',
+        backgroundColor: 'red',
+        position: 'topRight',
+        message: `❌ Rejected promise in ${ms}ms`,
+      });
+    });
+  form.reset();
+});
+
+function promiseGenerator(ms, state) {
+  return new Promise((resolve, reject) => {
+    if (state) {
+      setTimeout(() => {
+        resolve(ms);
+      }, ms);
+    } else {
+      setTimeout(() => {
+        reject(ms);
+      }, ms);
+    }
+  });
+}
